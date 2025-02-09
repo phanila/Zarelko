0e6f441dfd2eebe6222a1e2ac067c328ac9ae33b
weronika <weronika@example.com>
2025-02-09 19:07:06 +0100

    before database

--- lib/add_page.dart
+++ lib/add_page.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 import 'main.dart';
@@ -9,41 +10,137 @@ class AddPage extends StatefulWidget {
     return _AddPageState();
   }
 }
-class _AddPageState extends State {
+class _AddPageState extends State<AddPage> {
   final _formGlobalKey = GlobalKey<FormState>();
-  // String _name;
-  // String _desc;
-  // String _type;
-  // String _location;
-  // DateTime _expiryDate;
+  String _name = '';
+  String _desc = '';
+  String _type = '';
+  String _location = '';
+  DateTime _expiryDate = DateTime.now().add(Duration(days: 7));
+
+  TextEditingController _controlDate = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     return Scaffold(
+        resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text("Add food"),
       ),
       body: Center(
-          child: Form(
-            key: _formGlobalKey,
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+              child: Form(
+                key: _formGlobalKey,
             child: Column(
               children: [
                 // name
+                TextFormField(
+                  maxLength: 20,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    label: Text("Name")
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'You must enter a name';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _name = value!;
+                  },
+                ),
                 // desc
+                TextFormField(
+                  maxLength: 20,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text("Description")
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'You must enter a description';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _desc = value!;
+                  },
+                ),
                 // type
+                TextFormField(
+                  maxLength: 20,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text("Type")
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'You must enter a type';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _type = value!;
+                  },
+                ),
                 // location
+                TextFormField(
+                  maxLength: 20,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text("Location")
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'You must enter a location';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _location = value!;
+                  },
+                ),
                 // date
+                TextFormField(
+                  controller: _controlDate,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text("Date")
+                    ),
+                  onTap:() async {
+                    DateTime? pickedDate= await showDatePicker(
+                        context: context,
+                        firstDate: DateTime.now().subtract(Duration(days: 365*5)),
+                        lastDate: DateTime.now().add(Duration(days: 365*10))
+                    );
+                    if (pickedDate != null) {
+                      _expiryDate = pickedDate;
+                      setState(() {
+                        _controlDate.text = DateFormat("dd-MM-yyyy").format(pickedDate);
+                      });
+                    }
+                  }
+                ),
                 // submit button
                 const SizedBox(height: 20,),
                 FilledButton(
-                    onPressed: (){},
+                    onPressed: (){
+                      if (_formGlobalKey.currentState!.validate()) {
+                        _formGlobalKey.currentState!.save();
+                        appState.addFood(Food(_name, _desc, _expiryDate, _type, _location));
+                        Navigator.pop(context,_name);
+                      }
+                    },
                     child: const Text("Add")
                 )
               ],
             ),
           )
+      )
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,12 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Zarelko',
+        title: 'Żarełko',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
-        home: MyHomePage(title:'Zarelko'),
+        home: MyHomePage(title:'Żarełko'),
       ),
     );
   }
@@ -31,16 +31,16 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   var foodList = <Food>[];
 
-  MyAppState() {
-    foodList.add(Food(
-        'test', 'testowy', DateTime.parse('1969-07-20 20:18:04Z'), "TestType",
-        "Nowhere"));
-    foodList.add(Food(
-        'test2', 'testowy', DateTime(2025,02,13), "TestType",
-        "Nowhere"));
-    foodList.add(Food(
-        'test3', 'testowy', DateTime.parse('2026-07-20 20:18:04Z'), "TestType",
-        "Nowhere"));
+   MyAppState() {
+  //   foodList.add(Food(
+  //       'test', 'testowy', DateTime.parse('1969-07-20 20:18:04Z'), "TestType",
+  //       "Nowhere"));
+  //   foodList.add(Food(
+  //       'test2', 'testowy', DateTime(2025,02,13), "TestType",
+  //       "Nowhere"));
+  //   foodList.add(Food(
+  //       'test3', 'testowy', DateTime.parse('2026-07-20 20:18:04Z'), "TestType",
+  //       "Nowhere"));
   }
   void addFood(Food food) {
     foodList.add(food);
@@ -71,7 +71,14 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
       ),
-      body: Center(
+      body: foodList.isEmpty ? Center(
+          child: Padding(
+              padding: const EdgeInsets.all(20),
+              child:Image(image: AssetImage("assets/nofood.jpg")
+              )
+            )
+          ) :
+      Center(
       child: ListView.builder(
         itemCount: foodList.length,
         itemBuilder: (BuildContext context, int index) {
@@ -81,12 +88,7 @@ class MyHomePage extends StatelessWidget {
     ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => AddPage(),
-            ),
-          );
+          _navigateAndDisplayAddPage(context);
         },
         child: const Icon(Icons.add),
       ),
@@ -99,6 +101,7 @@ class FoodListTile extends StatelessWidget {
   final Food food;
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
     var color = Colors.white;
     if (DateTime.now().isAfter(food.expiryDate)){
       color = Colors.red;
@@ -112,9 +115,31 @@ class FoodListTile extends StatelessWidget {
           padding: const EdgeInsets.all(20),
 
           child: ExpansionTile(
-            title:Text(food.name.capitalize(),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 22),),
+            title:Row(children: [Text(food.name.capitalize(),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 22),),Expanded(
+              child: Container(),
+            ),
+              Text(DateFormat("E dd.MM.yyyy").format(food.expiryDate))]),
             subtitle: Text(food.type),
-            trailing:Text(DateFormat("E dd.MM.yyyy").format(food.expiryDate)),
+            trailing: IconButton(onPressed: ()  => showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: const Text('Are you sure?'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: const Text('No'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        appState.deleteFood(food);
+                        Navigator.pop(context);},
+                      child: const Text('Yes'),
+                    ),
+                  ],
+                ),
+            ),
+                    icon: Icon(Icons.delete_outline)
+              ),
             children: [
             Text(food.desc),
               Text(food.location)
@@ -132,4 +157,21 @@ class Food {
   final DateTime expiryDate;
   Food(this.name, this.desc, this.expiryDate, this.type,this.location);
 
+}
+Future<void> _navigateAndDisplayAddPage(BuildContext context) async {
+
+  final result = await Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => AddPage()),
+  );
+
+  if (!context.mounted) return;
+
+  // After the Selection Screen returns a result, hide any previous snackbars
+  // and show the new result.
+  if(result != null) {
+    ScaffoldMessenger.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(SnackBar(content: Text('Added food: $result')));
+  }
 }
